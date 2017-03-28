6d7c07cde8207d39e5ced0e16a1ebcf4c9d2b723
iJungleboy <daniel.mettler@2sic.com>
2017-03-28 21:59:24 +0200

    initial version - working :)!

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Hello Angular on DNN from 2sxc in Switzerland!';
+}
